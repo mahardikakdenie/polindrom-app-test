1a63c36e446f29c57a5c0e9f2e8f8bdf1a1ad2c9
MahardiikaKD <dikamahar884@gmail.com>
2024-12-18 17:42:34 +0700

    initial commit

--- src/libs/polindroms.ts
+++ src/libs/polindroms.ts
@@ -1,3 +1,21 @@
-// export const createData = (params, cb, errCb) => {
-//     // 
-// };
+import client from "./http-client";
+
+export const createData = (params: any, cb: any, errCb: any) => {
+    client.post('polindrom', params)
+        .then((res: any) => {
+            cb(res);
+        })
+        .catch(e => {
+            errCb(e);
+        })
+};
+
+export const getData = (params: any, cb: any, errCb: any) => {
+    client.post('polindrom', {params})
+        .then((res: any) => {
+            cb(res);
+        })
+        .catch(e => {
+            errCb(e);
+        })
+};
